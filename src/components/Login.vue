80cc07fbf610408941a8910f4a711deb2f7a8625
loc <loctruong96@gmail.com>
2020-02-13 16:41:25 -0800

    add required field functionality

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -43,6 +43,15 @@
                     <label for="password2">Password</label>
                     <input v-model.trim="signupForm.password" type="password" placeholder="min 6 characters" id="password2" />
 
+                    <label for="city">City</label>
+                    <input v-model.trim="signupForm.city" type="text" placeholder="" id="city" />
+
+                    <label for="state">State</label>
+                    <input v-model.trim="signupForm.state" type="text" placeholder="" id="state" />
+
+                    <label for="country">Country</label>
+                    <input v-model.trim="signupForm.country" type="text" placeholder="" id="country" />
+
                     <button @click="signup" class="button">Sign Up</button>
 
                     <div class="extras">
@@ -102,7 +111,59 @@
                 showForgotPassword: false,
                 passwordResetSuccess: false,
                 performingRequest: false,
-                errorMsg: ''
+                errorMsg: '',
+                interests: ["education","legal", "food"],
+                countries: ["United States"],
+                states: {"United States": ["Alabama",
+                    "Alaska",
+                    "Arizona",
+                    "Arkansas",
+                    "California",
+                    "Colorado",
+                    "Connecticut",
+                    "Delaware",
+                    "Florida",
+                    "Georgia",
+                    "Hawaii",
+                    "Idaho",
+                    "Illinois",
+                    "Indiana",
+                    "Iowa",
+                    "Kansas",
+                    "Kentucky",
+                    "Louisiana",
+                    "Maine",
+                    "Maryland",
+                    "Massachusetts",
+                    "Michigan",
+                    "Minnesota",
+                    "Mississippi",
+                    "Missouri",
+                    "Montana",
+                    "Nebraska",
+                    "Nevada",
+                    "New Hampshire",
+                    "New Jersey",
+                    "New Mexico",
+                    "New York",
+                    "North Carolina",
+                    "North Dakota",
+                    "Ohio",
+                    "Oklahoma",
+                    "Oregon",
+                    "Pennsylvania",
+                    "Rhode Island",
+                    "South Carolina",
+                    "South Dakota",
+                    "Tennessee",
+                    "Texas",
+                    "Utah",
+                    "Vermont",
+                    "Virginia",
+                    "Washington",
+                    "West Virginia",
+                    "Wisconsin",
+                    "Wyoming"]}
             }
         },
         methods: {
@@ -135,29 +196,49 @@
                 })
             },
             signup() {
-                this.performingRequest = true
+                if(!this.signupForm.name || !this.signupForm.city || !this.signupForm.state || !this.signupForm.country){
+                    this.performingRequest = false;
+                    if(!this.signupForm.name){
+                        this.errorMsg = "Name is required."
+                    } else if(!this.signupForm.city){
+                        this.errorMsg = "City is required."
+                    } else if(!this.signupForm.state){
+                        this.errorMsg = "State is required."
+                    } else if(!this.signupForm.country){
+                        this.errorMsg = "Country is required."
+                    }
+                    // this.errorMsg = "one of the missing fields are required."
+                } else {
+                    this.performingRequest = true;
 
-                fb.auth.createUserWithEmailAndPassword(this.signupForm.email, this.signupForm.password).then(user => {
-                    this.$store.commit('setCurrentUser', user.user)
+                    fb.auth.createUserWithEmailAndPassword(this.signupForm.email, this.signupForm.password).then(user => {
+                        this.$store.commit('setCurrentUser', user.user);
 
-                    // create user obj
-                    fb.usersCollection.doc(user.user.uid).set({
-                        name: this.signupForm.name,
-                        title: this.signupForm.title
-                    }).then(() => {
-                        this.$store.dispatch('fetchUserProfile')
-                        this.performingRequest = false
-                        this.$router.push('/dashboard')
+
+                        // create user obj
+                        fb.usersCollection.doc(user.user.uid).set({
+                            name: this.signupForm.name,
+                            title: this.signupForm.title,
+                            city: this.signupForm.city,
+                            state: this.signupForm.state,
+                            country: this.signupForm.country
+
+                        }).then(() => {
+                            this.$store.dispatch('fetchUserProfile')
+                            this.performingRequest = false
+                            this.$router.push('/dashboard')
+                        }).catch(err => {
+                            console.log(err)
+                            this.performingRequest = false
+                            this.errorMsg = err.message
+                        })
                     }).catch(err => {
                         console.log(err)
                         this.performingRequest = false
                         this.errorMsg = err.message
                     })
-                }).catch(err => {
-                    console.log(err)
-                    this.performingRequest = false
-                    this.errorMsg = err.message
-                })
+                }
+
             },
             resetPassword() {
                 this.performingRequest = true
