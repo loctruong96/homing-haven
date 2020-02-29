ffff5f76cea55fccb9805e6186352411b968a527
loc <loctruong96@gmail.com>
2020-02-28 23:01:02 -0800

    responsive navigation

--- src/components/UNavigation.vue
+++ src/components/UNavigation.vue
@@ -6,7 +6,12 @@
                  text-color="#fff"
                  :router="true"
                  active-text-color="#ffd04b">
-            <el-menu-item style="font-size: 30px">HomingHaven</el-menu-item>
+            <el-menu-item style="font-size: 30px" class="hide-nav">HomingHaven</el-menu-item>
+            <el-menu-item style="width: 50vw; min-width: 200px">
+                <el-input placeholder="Search for communities or resources..." v-model="search">
+                    <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+            </el-menu-item>
             <el-submenu index="4" style="float: right; font-size: 50px; color: #FFFFFF">
                 <template slot="title"><i class="el-icon-menu"  style="color: #FFFFFF"></i></template>
                 <el-menu-item index="4-1" :route="{name:'Login'}" style="color: #FFFFFF" ><i class="el-icon-user"  style="color: #FFFFFF"></i> Sign In</el-menu-item>
@@ -15,11 +20,17 @@
             <el-menu-item index="1" style="float: right; color: #FFFFFF" :route="{name:'Registration'}" class="hide-nav">Registration</el-menu-item>
             <el-menu-item index="2" style="float: right; color: #FFFFFF" :route="{name:'Login'}" class="hide-nav">Sign In</el-menu-item>
         </el-menu>
+
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                search:''
+            }
+        },
         methods: {
             login() {
                 this.$router.push('/login')
@@ -37,4 +48,11 @@
         left: initial !important;
         right: 0;
     }
+    .el-input {
+        width: 200px;
+    }
+    .input-with-select .el-input-group__prepend {
+        background-color: #fff;
+    }
+
 </style>
